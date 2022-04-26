99e75c8f4a65a921cc5f2f3749780a4379374687
Almas2442 <54279211+Almas2442@users.noreply.github.com>
2022-04-26 23:37:51 +0700

    typo

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import Welcome from "../pages/Welcome";
 import Login from "../pages/Login";
 import RegisterArtist from "../pages/artist/Register"
-import RegisterOwner from "../pages/owner/Register";
+import RegisterOwner from "../pages/owner/register";
 
 import CompleteOwner from "../pages/owner/CompleteOwner";
 import HomeOwner from "../pages/owner/Home";
