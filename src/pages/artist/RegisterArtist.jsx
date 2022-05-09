828e8fb1886cea3f2f449499a8d859ebc588d7d4
Almas2442 <54279211+Almas2442@users.noreply.github.com>
2022-05-09 08:31:32 +0700

    delete typo

--- src/pages/artist/RegisterArtist.jsx
+++ src/pages/artist/RegisterArtist.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
