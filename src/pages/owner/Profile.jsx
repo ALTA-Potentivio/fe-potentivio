e6d3a79bae09e46309ac74ac650ee2162c254a71
Almas2442 <54279211+Almas2442@users.noreply.github.com>
2022-05-12 23:50:02 +0700

    bug peta

--- src/pages/owner/Profile.jsx
+++ src/pages/owner/Profile.jsx
@@ -13,11 +13,14 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass =
-  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+// mapboxgl.workerClass =
+//   require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
+
+// mapboxgl.MAPBOX_TOKEN =
+//   "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
 const Profile = () => {
   const base_url = useSelector((state) => state.base_url);
